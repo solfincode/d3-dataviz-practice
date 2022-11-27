6b396d06daf00b4c566b16aae7d55efc14b2e36a
duo <hypersonolabs@gmail.com>
2022-11-27 23:01:45 +0900

    update line and barchart url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 //components
 import Card from "../components/Card";
@@ -14,8 +15,19 @@ export default function Home() {
           <p>this is practice for D3 on react framework</p>
         </div>
       </div>
-      <div className="flex flex-row mx-auto w-11/12 mt-20 mb-20">
-        <Card title="Line Chart" body="this is sample of line chart using d3" />
+      <div className="grid grid-cols-3 gap-4 mt-20 mb-20 mx-10">
+        <Link href="/viz/LineChart">
+          <Card
+            title="Line Chart"
+            body="this is sample of line chart using d3"
+          />
+        </Link>
+        <Link href="/viz/BarChart">
+          <Card title="Bar Chart" body="this is sample of bar chart using d3" />
+        </Link>
+        <Link href="">
+          <Card title="sample" body="this is sample of line chart using d3" />
+        </Link>
       </div>
     </>
   );
